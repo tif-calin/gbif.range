d3d270a949e4171e7773c8a7a33715133bd89c53
culi <culitif@tuta.io>
2025-07-30 21:31:34 -0700

    nit: fix comment spacing

--- R/get_range.R
+++ R/get_range.R
@@ -11,7 +11,7 @@
 #' buffer around them, but the buffer size increases with the number of points in the line.
 #' If there are too many records, get_range can process a sub-sample of species observations to
 #' speed up polygon creation or avoid potential RAM issues.
-#' 
+#'
 #' @param occ_coord a get_gbif() output or a data.frame containing two columns named
 #' "decimalLongitude" and "decimalLatitude".
 #' @param bioreg  Define the range extent and ecoregions. 'SpatialPolygonsDataFrame', 'SpatVector' or 'sf' object
@@ -50,51 +50,51 @@
 #' dynamics, and environmental conditions. The biodiversity of flora, fauna and ecosystems that
 #' characterise an ecoregion tends to be distinct from that of other ecoregions
 #' (https://en.wikipedia.org/wiki/Ecoregion).
-#' 
+#'
 #' Each ecoregion shapefile has one or more categories, which describe more or less precisely the
 #' ecoregion world distribution (from the more to the less detailed):
-#' 
+#'
 #' - eco_terra has three different levels: 'ECO_NAME', 'WWF_MHTNAM' and 'WWF_REALM2'.
 #' - eco_fresh has only one: 'ECOREGION'.
 #' - eco_marine and eco_hd_marine (very coastal-precise version) contains three distinct levels:
 #' 'ECOREGION', 'PROVINCE' and 'REALM'.
-#' 
+#'
 #' @return A 'SpatVector' or 'SpatRaster' + the list of all arguments used in the function.
 #' @references
 #' Oskar Hagen, Lisa Vaterlaus, Camille Albouy, Andrew Brown, Flurin Leugger, Renske E. Onstein,
 #' Charles Novaes de Santana, Christopher R. Scotese, Loïc Pellissier. (2019) Mountain building,
 #' climate cooling and the richness of cold-adapted plants in the Northern Hemisphere. Journal of
 #' Biogeography. doi: 10.1111/jbi.13653
-#' 
-#' Lyu, L., Leugger, F., Hagen, O., Fopp, F., Boschman, L. M., Strijk, J. S., ... & 
+#'
+#' Lyu, L., Leugger, F., Hagen, O., Fopp, F., Boschman, L. M., Strijk, J. S., ... &
 #' Pellissier, L. (2022). An integrated high resolution mapping shows congruent biodiversity
 #' patterns of Fagales and Pinales. New Phytologist, 235(2), 759-772 10.1111/nph.18158
-#' 
+#'
 #' Olson, D. M., Dinerstein, E., Wikramanayake, E. D., Burgess, N. D., Powell, G. V. N.,
 #' Underwood, E. C., D'Amico, J. A., Itoua, I., Strand, H. E., Morrison, J. C., Loucks, C. J.,
 #' Allnutt, T. F., Ricketts, T. H., Kura, Y., Lamoreux, J. F., Wettengel, W. W., Hedao, P., Kassem,
 #' K. R. 2001. Terrestrial ecoregions of the world: a new map of life on Earth.
 #' Bioscience 51(11):933-938. doi: 10.1641/0006-3568(2001)051
-#' 
+#'
 #' The Nature Conservancy (2009). Global Ecoregions, Major Habitat Types, Biogeographical Realms
 #' and The Nature Conservancy Terrestrial Assessment Units. GIS layers developed by The Nature
 #' Conservancy with multiple partners, combined from Olson et al. (2001), Bailey 1995 and Wiken 1986.
 #' Cambridge (UK): The Nature Conservancy.
-#' 
+#'
 #' Mark D. Spalding, Helen E. Fox, Gerald R. Allen, Nick Davidson, Zach A. Ferdaña, Max
 #' Finlayson, Benjamin S. Halpern, Miguel A. Jorge, Al Lombana, Sara A. Lourie, Kirsten D.
 #' Martin, Edmund McManus, Jennifer Molnar, Cheri A. Recchia, James Robertson, Marine
 #' Ecoregions of the World: A Bioregionalization of Coastal and Shelf Areas, BioScience,
 #' Volume 57, Issue 7, July 2007, Pages 573–583. doi: 10.1641/B570707
-#' 
+#'
 #' Spalding, M. D., Agostini, V. N., Rice, J., & Grant, S. M. (2012). Pelagic provinces of the world:
 #' a biogeographic classification of the world’s surface pelagic waters. Ocean & Coastal Management,
 #' 60, 19-30. doi: 10.1016/j.ocecoaman.2011.12.016
-#' 
+#'
 #' The Nature Conservancy (2012). Marine Ecoregions and Pelagic Provinces of the World. GIS layers
 #' developed by The Nature Conservancy with multiple partners, combined from Spalding et al. (2007)
 #' and Spalding et al. (2012). Cambridge (UK): The Nature Conservancy.
-#' 
+#'
 #' Robin Abell, Michele L. Thieme, Carmen Revenga, Mark Bryer, Maurice Kottelat, Nina Bogutskaya,
 #' Brian Coad, Nick Mandrak, Salvador Contreras Balderas, William Bussing, Melanie L. J. Stiassny,
 #' Paul Skelton, Gerald R. Allen, Peter Unmack, Alexander Naseka, Rebecca Ng, Nikolai Sindorf,
@@ -103,7 +103,7 @@
 #' Paulo Petry, Freshwater Ecoregions of the World: A New Map of Biogeographic Units for
 #' Freshwater Biodiversity Conservation, BioScience, Volume 58, Issue 5, May 2008,
 #' Pages 403–414. doi: 10.1641/B580507
-#' 
+#'
 #' Hijmans, Robert J. "terra: Spatial Data Analysis. R Package Version 1.6-7." (2022). Terra - CRAN
 #' @seealso
 #' For more information on the original code and methods, check Hagen, Oskar et al. (2019), Data
@@ -118,13 +118,13 @@
 #' @importFrom mclust Mclust mclustBIC
 #' @importFrom ClusterR KMeans_rcpp
 #' @export
-get_range <- function (occ_coord = NULL, 
-                       bioreg = NULL, 
-                       bioreg_name = NULL, 
+get_range <- function (occ_coord = NULL,
+                       bioreg = NULL,
+                       bioreg_name = NULL,
                        degrees_outlier = 5,
                        clustered_points_outlier = 3,
-                       buffer_width_point = 4, 
-                       buffer_increment_point_line = 0.5, 
+                       buffer_width_point = 4,
+                       buffer_increment_point_line = 0.5,
                        buffer_width_polygon = 4,
                        dir_temp = tempdir(),
                        raster = TRUE,
@@ -146,19 +146,19 @@ get_range <- function (occ_coord = NULL,
   # Bioreg name
   if (names(bioreg)[1] %in% "CLARA") {
     bioreg_name <- "EcoRegion"
-  } 
+  }
   if (methods::is(bioreg_name, "NULL")) {
     stop("Name of the desired ecoregion level/category ('bioreg_name' parameter) is missing, please provide one...")
-  } 
+  }
 
   # occ_coord
   if (!methods::is(occ_coord, "data.frame")) {
     stop("'occ_coord' is not a data.frame...")
-  } 
+  }
   if (!any(names(occ_coord) %in% "decimalLongitude")) {
     stop("Longitute/Latitude columns wrongly defined...")
   }
-  
+
   # get sp.name, and stop if not unique
   sp.name <- unique(occ_coord$input_search)
   if (length(sp.name) > 1) {
@@ -177,44 +177,44 @@ get_range <- function (occ_coord = NULL,
   ### =========================================================================
   ### Check if sufficient data
   ### =========================================================================
-  
+
   # Check if there sufficient species & if not, make an entry in the log-file and end the function
   if (nrow(occ_coord) <= clustered_points_outlier +1 ){
     stop("Too few occurences!")
-  } 
-  
-  if (verbose){
-    cat("## Start of computation for species: ", sp.name, " ###", "\n") 
   }
-  
+
+  if (verbose){
+    cat("## Start of computation for species: ", sp.name, " ###", "\n")
+  }
+
   ### =========================================================================
   ### Identify outliers
   ### =========================================================================
-  
+
   # Create distance matrix...
   mat.dist <- as.matrix(FNN::knn.dist(terra::crds(occ_coord), k = clustered_points_outlier))
-  
+
   # Mark outliers
   cond <- apply(mat.dist, 1, function(x) x[clustered_points_outlier]) > degrees_outlier
-  
+
   # Print info
   if (verbose){
     cat(paste0(sum(cond), " outlier's from " ,nrow(occ_coord), " | proportion from total points: ",
       round((sum(cond) / nrow(occ_coord)) * 100,0), "%\n"))
   }
-  
+
   # Remove outliers
   occ.coord.mod <- occ_coord[!cond, ]
 
   # Stop if too many outliers in data set
   if(nrow(occ.coord.mod) == 0){
     stop('Too few occurrences within outlier threshold!')
-  } 
+  }
 
   ### =========================================================================
   ### Define those polygons
   ### =========================================================================
-  
+
   # Set number of ecoregions
   ovo.coord.mod <- terra::intersect(bioreg,occ.coord.mod)
   uniq <- levels(factor(ovo.coord.mod[[bioreg_name]][[1]]))
@@ -223,14 +223,14 @@ get_range <- function (occ_coord = NULL,
   if (length(ovo.coord.mod) == 0) {
     stop("No overlap of ecoregion info, please use another 'bioreg_name'")
   }
-  
+
   # Loop over bioregions
   SP.dist <- list()
   for(g in 1:length(uniq)) {
-    
+
     # Print
     if (verbose){
-      cat('bioregion', g, ' of ',length(uniq),": ",uniq[g], '\n')
+      cat('bioregion ', g, ' of ', length(uniq), ": ",uniq[g], '\n', sep = "")
     }
 
     # NAs or not
@@ -240,38 +240,38 @@ get_range <- function (occ_coord = NULL,
     # Continue
     tmp <- terra::simplifyGeom(bioreg[q1, ], tolerance = 0.001, preserveTopology = TRUE)
     a <- ovo.coord.mod[ovo.coord.mod[[bioreg_name]][[1]] == uniq[g]]
-    
+
     if (length(a) < 3) {
       k <- 1
       cluster.k <- stats::kmeans(terra::crds(a), k)
-      cluster.k$clusters <- cluster.k$cluster 
+      cluster.k$clusters <- cluster.k$cluster
 
     } else {
 
       if (all(terra::crds(a)[, 1] == terra::crds(a)[, 2])) {
         terra::crds(a)[, 2] <- terra::crds(a)[, 2] + 0.00001
       }
-      
+
       # Determine number of clusters
       m.clust <- mclust::Mclust(terra::crds(a) + 1000, verbose = FALSE)
-      
+
       # k = number of clusters
-      k <- m.clust$G 
-      
+      k <- m.clust$G
+
       # Reduce k if necessary so that KMeans_rcpp() will run
-      while (k > length(a)-2) {k <- k-1} 
+      while (k > length(a)-2) {k <- k-1}
       if (k == 0) {k <- 1}
-      
+
       cluster.k <- ClusterR::KMeans_rcpp(terra::crds(a), k, num_init = 20, initializer = 'random')
-      
+
       while (length(unique(cluster.k$clusters)) < k) {
         k <- k-1
         cluster.k <- ClusterR::KMeans_rcpp(terra::crds(a), k, num_init = 20, initializer = 'random')
       }
-      
+
     }
-   
-    polygons.list <- list() 
+
+    polygons.list <- list()
     for (i in 1:k)
     {
       # kmeans (with number of clusters from mcluster)
@@ -284,26 +284,26 @@ get_range <- function (occ_coord = NULL,
                             bwpo = buffer_width_polygon,
                             temp_dir = dir_temp,
                             g = g)
-      
+
       # Intersect polygon with ecoregion (zero buffer to avoid error)
       b1 <- terra::buffer(my.shpe, width = 0)
       b2 <- terra::buffer(tmp, width = 0)
       polygons.list[[i]] <- terra::intersect(b1, b2)
-    }  
-    
+    }
+
     SP.dist[[g]] <- do.call("rbind", polygons.list)
-  } 
-  
+  }
+
   lala <- SP.dist[!is.na(SP.dist)]
-  
+
   ### =========================================================================
   ### Check and return output
   ### =========================================================================
-  
+
   if (!dir.exists(dir_temp)) {
     unlink(dir_temp, recursive = TRUE)
   }
-  
+
   if (length(lala) == 0) {
     stop('No occurrences within Bioregions. Empty raster produced...')
   }
@@ -317,7 +317,7 @@ get_range <- function (occ_coord = NULL,
     ras <- terra::rasterize(sp.range.u, ras.res)
     shp.species <- terra::crop(ras, sp.range.u)
   }
-  
+
   # Final print
   if (verbose){
     cat("## End of computation for species: ", sp.name, " ###", "\n")
@@ -327,7 +327,7 @@ get_range <- function (occ_coord = NULL,
   result <- list(init.args = list(
                   occ_coord = occ.coord.k,
                   bioreg = bioreg,
-                  bioreg_name = bioreg_name, 
+                  bioreg_name = bioreg_name,
                   degrees_outlier = degrees_outlier,
                   clustered_points_outlier = clustered_points_outlier,
                   buffer_width_point = buffer_width_point,
